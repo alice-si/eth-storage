50c841229356091373f065b4aaccc17bc3713c75
unknown <jabone@akhaakha.com>
2018-07-24 12:26:01 +0200

    push

---
+++ tests/storageMultiple.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var StateDB = require('./../state.js');
+var Settings = require('./settings.js');
+
+var stateDB = new StateDB(Settings.dbPath);
+var sampleAdress = stateDB.bufferHex('6badc9463c5cc91cbfb5176ef99a454c3c77b00e');
+console.log('sample adress:', sampleAdress);
+var number = stateDB.buffer64(1117760);
+var number2 = stateDB.buffer64(1117810);
+var index=stateDB.buffer256(0)
+stateDB.getMultiple(sampleAdress, number, number2, index, 5, function (err, storage) {
+    console.log('storage at index(',index,'):\n', storage);
+});
