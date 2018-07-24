2163151ca4231d9bd7971768f13981facb047e38
unknown <jabone@akhaakha.com>
2018-07-24 12:41:12 +0200

    typo

--- tests/getIndex.js
+++ tests/getIndex.js
@@ -1,10 +1,11 @@
 var assert = require('assert');
 
-var Trie = require('../../merkle-patricia-tree');
 var StateDB = require('./../state.js');
+var Settings = require('./settings.js');
+
+var stateDB = new StateDB(Settings.dbPath);
 
 // var stateDB = new StateDB('./../geth/chaindata');
-var stateDB = new StateDB('C:/Users/ja1/Alice/dirforfullrinkeby/geth/chaindata');
 var sampleAdress = stateDB.bufferHex('6badc9463c5cc91cbfb5176ef99a454c3c77b00e');
 console.log('sample adress:', sampleAdress);
 var number = stateDB.buffer64(1000000);
