9117ddc53ee9091fb61e7b1f1abe5eace6f5c71b
unknown <jabone@akhaakha.com>
2018-08-05 16:32:37 +0200

    ethStorage

---
+++ test/benchmark/ethStorage.js
@@ -0,0 +1,11 @@
+var StateDB = require('../../ethStorage.js');
+var Settings = require('./../settings.js');
+
+var stateDB = new StateDB(Settings.dbPath);
+
+for (var i = 0; i < Settings.getRangeTests.length; i++) { // goes through test cases
+    var testCase = Settings.getRangeTests[i];
+    stateDB.getRange(testCase.adr, testCase.idx, testCase.startBlock, testCase.endBlock, Settings.newTimeCb(testCase));
+}
+
+
