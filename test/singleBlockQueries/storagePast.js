392e61f532512d033619fa94ef99d9b859d984be
unknown <jabone@akhaakha.com>
2018-08-07 06:26:25 +0200

    new codes

--- test/singleBlockQueries/storagePast.js
+++
@@ -1,8 +0,0 @@
-var StateDB = require('../../stateSingleQueries.js');
-var Settings = require('../settings.js');
-
-var stateDB = new StateDB(Settings.dbPath);
-
-stateDB.getStorage('6badc9463c5cc91cbfb5176ef99a454c3c77b00e', 0xF4240, function (err, storage) {
-    console.log('storage:', storage);
-});
