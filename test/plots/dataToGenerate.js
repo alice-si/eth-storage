16ed879393e78fdb39eaa4682ac51c379f40176a
unknown <jabone@akhaakha.com>
2018-08-30 18:36:07 +0200

    bef genereating

--- test/plots/dataToGenerate.js
+++ test/plots/dataToGenerate.js
@@ -1,78 +1,77 @@
 var getRangeTests = [
 
-    {
-        adr: "0xcd56b102622125B62E7acEEdA08D393cA0cc28Fc",
-        idx: 0,
-        startBlock: 2700000,
-        endBlock: 2700500,
-        msg: 'this is sample 500 blocks query, should run fast'
-    },
     // {
     //     adr: "0xcd56b102622125B62E7acEEdA08D393cA0cc28Fc",
     //     idx: 0,
     //     startBlock: 2700000,
-    //     endBlock: 2705000,
-    //     expectedOutput: '',
-    //     msg: '5`000 blocks, contract 0xcd56b102... at index 0'
-    // },
-    // {
-    //     adr: "0xcd56b102622125B62E7acEEdA08D393cA0cc28Fc",
-    //     idx: 0,
-    //     startBlock: 2700000,
-    //     endBlock: 2750000,
-    //     cb: testLog,
-    //     expectedOutput: '',
-    //     msg: '50`000 blocks, contract 0xcd56b102... at index 0'
+    //     endBlock: 2700500,
+    //     msg: 'this is sample 500 blocks query, should run fast'
     // },
 
-    // {
-    //     adr: "0x16c8985321696c21d58f3194eee166eedaf37356",
-    //     idx: 0,
-    //     startBlock: 2770000,
-    //     endBlock: 2790000,
-    //     expectedOutput: '',
-    //     msg: '20000 blocks, samplecontract 1 (1000 changes to 10000 blocks) at index 0'
-    // },
-    // {
-    //     adr: "0xf0764a85241e2ad94a3c75e0a9a17d3ede3865ad",
-    //     idx: 0,
-    //     startBlock: 2770000,
-    //     endBlock: 2790000,
-    //     expectedOutput: '',
-    //     msg: '20000 blocks, samplecontract 2 (100 changes to 10000 blocks) at index 0'
-    // },
-    // {
-    //     adr: "0x68abe76de1aaaace161bae789a71e54183e1df8b",
-    //     idx: 0,
-    //     startBlock: 2770000,
-    //     endBlock: 2790000,
-    //     expectedOutput: '',
-    //     msg: '20000 blocks, samplecontract 3 (10 changes to 10000 blocks) at index 0'
-    // },
-    // {
-    //     adr: "0x16c8985321696c21d58f3194eee166eedaf37356",
-    //     idx: 2,
-    //     startBlock: 2770000,
-    //     endBlock: 2790000,
-    //     expectedOutput: '',
-    //     msg: '20000 blocks, samplecontract 1 (1000 changes to 10000 blocks) at index 2'
-    // },
-    // {
-    //     adr: "0xf0764a85241e2ad94a3c75e0a9a17d3ede3865ad",
-    //     idx: 2,
-    //     startBlock: 2770000,
-    //     endBlock: 2790000,
-    //     expectedOutput: '',
-    //     msg: '20000 blocks, samplecontract 2 (100 changes to 10000 blocks) at index 2'
-    // },
-    // {
-    //     adr: "0x68abe76de1aaaace161bae789a71e54183e1df8b",
-    //     idx: 2,
-    //     startBlock: 2770000,
-    //     endBlock: 2790000,
-    //     expectedOutput: '',
-    //     msg: '20000 blocks, samplecontract 3 (10 changes to 10000 blocks) at index 2'
-    // },
+
+    {
+        adr: "0x16c8985321696c21d58f3194eee166eedaf37356",
+        idx: 0,
+        startBlock: 2785000,
+        endBlock: 2795000,
+        msg: 'get sStorageInt, variable sStorageInt, sStorageByte and sStorageString where changed each 8 blocks'
+    },
+    {
+        adr: "0xf0764a85241e2ad94a3c75e0a9a17d3ede3865ad",
+        idx: 0,
+        startBlock: 2785000,
+        endBlock: 2795000,
+        msg: 'get sStorageInt, sStorageByte and sStorageString where changed each 80 blocks'
+    },
+    {
+        adr: "0x68abe76de1aaaace161bae789a71e54183e1df8b",
+        idx: 0,
+        startBlock: 2785000,
+        endBlock: 2795000,
+        msg: 'get sStorageInt, sStorageByte where changed each 800 blocks, sStorageString where changed each 8 blocks'
+    },
+    {
+        adr: "0x16c8985321696c21d58f3194eee166eedaf37356",
+        idx: 1,
+        startBlock: 2785000,
+        endBlock: 2795000,
+        msg: 'get sStorageByte, variable sStorageInt, sStorageByte and sStorageString where changed each 8 blocks'
+    },
+    {
+        adr: "0xf0764a85241e2ad94a3c75e0a9a17d3ede3865ad",
+        idx: 1,
+        startBlock: 2785000,
+        endBlock: 2795000,
+        msg: 'get sStorageByte, sStorageByte and sStorageString where changed each 80 blocks'
+    },
+    {
+        adr: "0x68abe76de1aaaace161bae789a71e54183e1df8b",
+        idx: 1,
+        startBlock: 2785000,
+        endBlock: 2795000,
+        msg: 'get sStorageByte, sStorageByte where changed each 800 blocks, sStorageString where changed each 8 blocks'
+    },
+    {
+        adr: "0x16c8985321696c21d58f3194eee166eedaf37356",
+        idx: 2,
+        startBlock: 2785000,
+        endBlock: 2795000,
+        msg: 'get sStorageString, variable sStorageInt, sStorageByte and sStorageString where changed each 8 blocks'
+    },
+    {
+        adr: "0xf0764a85241e2ad94a3c75e0a9a17d3ede3865ad",
+        idx: 2,
+        startBlock: 2785000,
+        endBlock: 2795000,
+        msg: 'get sStorageString, sStorageByte and sStorageString where changed each 80 blocks'
+    },
+    {
+        adr: "0x68abe76de1aaaace161bae789a71e54183e1df8b",
+        idx: 2,
+        startBlock: 2785000,
+        endBlock: 2795000,
+        msg: 'get sStorageString, sStorageByte where changed each 800 blocks, sStorageString where changed each 8 blocks'
+    },
 
 ];
 
